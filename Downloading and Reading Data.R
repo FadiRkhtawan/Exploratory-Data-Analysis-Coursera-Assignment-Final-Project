783bbce9ea7e7f8b01288bebc37638023c163d43
Fadi Rkhtawan <47181420+FadiRkhtawan@users.noreply.github.com>
2021-01-20 00:57:26 +0300

    Update and rename 2nd Assignment.R to Downloading and Reading Data.R

---
+++ Downloading and Reading Data.R
@@ -0,0 +1,24 @@
+
+## Selecting the directory
+
+setwd("D:/Fadi_Command/Course 4/Assignment 2")
+
+## Downloading the zip file frim the URL
+
+dir.create("./air_pollution")
+urlzip <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+download.file(urlzip, destfile = "./air_pollution.zip" )
+
+## Extracting the files from the compressed one
+
+unzip("./air_pollution.zip", exdir = "./air_pollution" )
+
+## Reading data
+
+NEI <- readRDS("./air_pollution/summarySCC_PM25.rds")
+SCC <- readRDS("./air_pollution/Source_Classification_Code.rds")
+
+## Testing Data
+
+str(NEI)
+str(SCC)
